b2504e47869dc5e9785d9c21f10001c3b60ecfd9
parsa <pramezani92@gmail.com>
2025-07-06 10:23:08 +0330

    fix: test file name

--- src/api/tests/views/authentication/__init__.py
+++ src/api/tests/views/authentication/__init__.py
@@ -1,7 +1,7 @@
 from src.api.tests.views.authentication.send_otp import (
     SendOneTimePasswordAPIViewTestCase,
 )
-from src.api.tests.views.authentication.verify_otp import (
+from src.api.tests.views.authentication.verify_token import (
     VerifyOneTimePasswordViewTestCase
 )
 from src.api.tests.views.authentication.refresh_token import (
