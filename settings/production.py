4c156fa56f44c3572486aee04f0ed9149e1f0e7e
Parsa <pramezani92@gmail.com>
2025-09-30 20:48:10 +0330

    fix: CORS HEADERS

--- settings/production.py
+++ settings/production.py
@@ -118,9 +118,9 @@ CSRF_COOKIE_DOMAIN = get_env("CSRF_COOKIE_DOMAIN")
 # CORS_ORIGIN_REGEX_WHITELIST = [
 #     re.compile(r) for r in get_env("CORS_ORIGIN_REGEX_WHITELIST").split(",")
 # ]
-CORS_REPLACE_HTTPS_REFERER = True
 CORS_ALLOW_CREDENTIALS = True
 # CORS_URLS_REGEX = re.compile(get_env("CORS_URLS_REGEX"))
+CORS_ALLOWED_ORIGINS = list(get_env("CORS_ALLOWED_ORIGINS").split(","))
 CSRF_COOKIE_SAMESITE = get_env("CSRF_COOKIE_SAMESITE")
 SESSION_COOKIE_SAMESITE = get_env("SESSION_COOKIE_SAMESITE")
 CSRF_COOKIE_SECURE = get_env("CSRF_COOKIE_SECURE")
@@ -129,7 +129,13 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTOCOL", "https")
 # END CORSHEADERS CONFIGURATION
 DEBUG = get_env("DEBUG") == "True"
 
-MIDDLEWARE += ("corsheaders.middleware.CorsMiddleware",)
+MIDDLEWARE = list(MIDDLEWARE)
+
+# Find index of CommonMiddleware
+index = MIDDLEWARE.index("django.middleware.common.CommonMiddleware")
+
+# Insert corsheaders middleware before it
+MIDDLEWARE.insert(index, "corsheaders.middleware.CorsMiddleware")
 
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
